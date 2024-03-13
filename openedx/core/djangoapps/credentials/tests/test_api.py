e4558cbd5ad8977c59c78c8451526677a984f883
Justin Hynes <Jhynes@edx.org>
2024-03-13 17:57:15 +0000

    fix: skip new unit tests unless they are run by the LMS

--- openedx/core/djangoapps/credentials/tests/test_api.py
+++ openedx/core/djangoapps/credentials/tests/test_api.py
@@ -8,6 +8,7 @@ from django.test import TestCase
 from openedx.core.djangoapps.credentials.api import is_credentials_enabled
 from openedx.core.djangoapps.credentials.models import CredentialsApiConfig
 from openedx.core.djangoapps.credentials.tests.mixins import CredentialsApiConfigMixin
+from openedx.core.djangolib.testing.utils import skip_unless_lms
 
 
 class CredentialsApiTests(CredentialsApiConfigMixin, TestCase):
@@ -18,6 +19,7 @@ class CredentialsApiTests(CredentialsApiConfigMixin, TestCase):
         super().setUp()
         CredentialsApiConfig.objects.all().delete()
 
+    @skip_unless_lms
     def test_is_credentials_enabled_config_enabled(self):
         """
         A test that verifies the output of the `is_credentials_enabled` utility function when a CredentialsApiConfig
@@ -26,6 +28,7 @@ class CredentialsApiTests(CredentialsApiConfigMixin, TestCase):
         self.create_credentials_config(enabled=True)
         assert is_credentials_enabled()
 
+    @skip_unless_lms
     def test_is_credentials_enabled_config_disabled(self):
         """
         A test that verifies the output of the `is_credentials_enabled` utility function when a CredentialsApiConfig
@@ -34,6 +37,7 @@ class CredentialsApiTests(CredentialsApiConfigMixin, TestCase):
         self.create_credentials_config(enabled=False)
         assert not is_credentials_enabled()
 
+    @skip_unless_lms
     def test_is_credentials_enabled_config_absent(self):
         """
         A test that verifies the output of the `is_credentials_enabled` utility function when a CredentialsApiConfig
