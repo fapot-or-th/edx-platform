f0441c0cb69827a853d231218aac491b26ccb953
nzinnocent <tanate.loj@gmail.com>
2024-04-24 14:46:34 +0700

    feat: inject course list api

--- lms/djangoapps/learner_home/urls.py
+++ lms/djangoapps/learner_home/urls.py
@@ -6,11 +6,13 @@ from django.urls import path
 from django.urls import include, re_path
 
 from lms.djangoapps.learner_home import views
+from lms.djangoapps.course_api.views import CourseListView
 
 app_name = "learner_home"
 
 # Learner Dashboard Routing
 urlpatterns = [
+    re_path(r"^init/courses/?", CourseListView.as_view(), name="initialize-course-list"),
     re_path(r"^init/?", views.InitializeView.as_view(), name="initialize"),
     path("mock/", include("lms.djangoapps.learner_home.mock.urls")),
 ]
